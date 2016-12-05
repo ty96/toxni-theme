87d11a670e7b44a264bc072c24e4045b64022eb3
Toxni <i@toxni.com>
2016-12-05 21:33:43 +0800

    feat(): adapt different devices

--- assets/js/countDown.js
+++ assets/js/countDown.js
@@ -1,32 +1,33 @@
 (function () {
   var box = document.getElementsByClassName('countDown')[0];
-  box.style.marginTop = window.innerHeight / 2 - 72 + 'px';
+  if (!!box) {
+    box.style.marginTop = window.innerHeight / 2 - 72 + 'px';
 
-  var
-    day = document.getElementsByClassName('day')[0],
-    hour = document.getElementsByClassName('hour')[0],
-    minute = document.getElementsByClassName('minute')[0],
-    second = document.getElementsByClassName('second')[0];
-
-  var startUp = 'Mon Jul 20 2015 20:00:00 GMT+0800 (CST)';
-  var startUpS = new Date(startUp).getTime();
-
-  var timer = function () {
-    var now = new Date().getTime();
-    var range = parseInt((now - startUpS) / 1000);
     var
-      s = range % 60,
-      m = ((range - s) / 60) % 60,
-      h = ((range - s - m * 60) / 3600) % 24,
-      d = ((range - s - m * 60 - h * 3600) / (3600 * 24));
+      day = document.getElementsByClassName('day')[0],
+      hour = document.getElementsByClassName('hour')[0],
+      minute = document.getElementsByClassName('minute')[0],
+      second = document.getElementsByClassName('second')[0];
 
-    second.innerText = s;
-    minute.innerText = m;
-    hour.innerText = h;
-    day.innerText = d;
-  };
+    var startUp = 'Mon Jul 20 2015 20:00:00 GMT+0800 (CST)';
+    var startUpS = new Date(startUp).getTime();
 
-  timer();
-  setInterval(timer, 1000)
+    var timer = function () {
+      var now = new Date().getTime();
+      var range = parseInt((now - startUpS) / 1000);
+      var
+        s = range % 60,
+        m = ((range - s) / 60) % 60,
+        h = ((range - s - m * 60) / 3600) % 24,
+        d = ((range - s - m * 60 - h * 3600) / (3600 * 24));
 
+      second.innerText = s;
+      minute.innerText = m;
+      hour.innerText = h;
+      day.innerText = d;
+    };
+
+    timer();
+    setInterval(timer, 1000)
+  }
 })();
--- assets/js/default.js
+++ assets/js/default.js
@@ -1,52 +1,88 @@
 ;(function () {
   var userAgentBox = document.getElementById('userAgent')
+  var ipBox = document.getElementById('ip')
+  var indexBox = document.getElementById('indexBox')
+  var catalog = document.getElementById('catalog')
+  var liList = catalog.parentNode.children
+
+
   var userAgentVal = navigator.userAgent
-  var ipBox = document.getElementById("ip")
   var ip = window.returnCitySN.cip
 
-  if (userAgentBox) {
+  if (!!userAgentBox) {
     userAgentBox.innerText = userAgentVal
   }
 
-  if (ipBox) {
+  if (!!ipBox) {
     document.getElementById("ip").innerText = ip
   }
 
+  if (!!indexBox) {
+    indexBox.style.marginTop = '50px'
+    indexBox.style.opacity = 1
+  }
+
+  ;(function () {
+    if (!!catalog && window.innerWidth <= 640) {
+      catalog.onclick = function () {
+        if (liList[1].style.display == 'none') {
+          for (var i = 1; i < liList.length; i++) {
+            liList[i].style.display = 'block'
+          }
+        }
+        else {
+          for (var i = 1; i < liList.length; i++) {
+            liList[i].style.display = 'none'
+          }
+        }
+      }
+    }
+  })()
+
+
   ;(function bgImage() {
     function A() {
-      t.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, t.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+      t.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+      t.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
     }
 
     function e() {
-      o.clearRect(0, 0, t.width, t.height);
-      var A = [a].concat(n);
+      o.clearRect(0, 0, t.width, t.height)
+      var A = [a].concat(n)
       n.forEach(function (e) {
-        e.x += e.xa, e.y += e.ya, e.xa *= e.x > t.width || e.x < 0 ? -1 : 1, e.ya *= e.y > t.height || e.y < 0 ? -1 : 1, o.fillRect(e.x - .5, e.y - .5, 1, 1);
+        e.x += e.xa
+        e.y += e.ya
+        e.xa *= e.x > t.width || e.x < 0 ? -1 : 1
+        e.ya *= e.y > t.height || e.y < 0 ? -1 : 1
+        o.fillRect(e.x - .5, e.y - .5, 1, 1)
         for (var i = 0; i < A.length; i++) {
-          var n = A[i];
+          var n = A[i]
           if (e !== n && null !== n.x && null !== n.y) {
             var r, l = e.x - n.x, s = e.y - n.y, d = l * l + s * s;
             d < n.max && (n === a && d >= n.max / 2 && (e.x -= .03 * l, e.y -= .03 * s), r = (n.max - d) / n.max, o.beginPath(), o.lineWidth = r / 2, o.strokeStyle = "rgba(0,0,0," + (r + .2) + ")", o.moveTo(e.x, e.y), o.lineTo(n.x, n.y), o.stroke())
           }
         }
         A.splice(A.indexOf(e), 1)
-      }), i(e)
+      })
+      i(e)
     }
 
-    var t = document.getElementById("cas"), o = t.getContext("2d");
-    A(), window.onresize = A;
+    var t = document.getElementById("cas"), o = t.getContext("2d")
+    A()
+    window.onresize = A
     var i = function () {
       return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (A) {
           window.setTimeout(A, 1e3 / 60)
         }
-    }(), a = {x: null, y: null, max: 2e4};
+    }(), a = {x: null, y: null, max: 2e4}
     window.onmousemove = function (A) {
       A = A || window.event, a.x = A.clientX, a.y = A.clientY
-    }, window.onmouseout = function (A) {
+    }
+    window.onmouseout = function (A) {
       a.x = null, a.y = null
-    };
+    }
     for (var n = [], r = 0; 150 > r; r++) {
-      var l = Math.random() * t.width, s = Math.random() * t.height, d = 2 * Math.random() - 1, c = 2 * Math.random() - 1;
+      var l = Math.random() * t.width, s = Math.random() * t.height, d = 2 * Math.random() - 1, c = 2 * Math.random() - 1
       n.push({x: l, y: s, xa: d, ya: c, max: 6e3})
     }
     setTimeout(function () {
